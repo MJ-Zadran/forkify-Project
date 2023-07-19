646767eb110d2842291ff49aec4a43ee54ffce57
MJ-Zadran <muhammadjamilzadran1010@gmail.com>
2023-07-19 10:53:40 -0700

    Added new Welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -123,6 +123,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log("Welcome to the forkify application");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandleRender(controlRecipes);
@@ -131,7 +135,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome");
+  newFeature();
 };
 
 init();
